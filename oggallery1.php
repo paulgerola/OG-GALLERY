5314531c51b20bc7942bd60900c8d95b8331c957
paulgerola <116621062+paulgerola@users.noreply.github.com>
2023-04-01 17:24:18 +0800

    Add files via upload

---
+++ oggallery1.php
@@ -0,0 +1,161 @@
+<html>
+<head><title>firegallery</title>
+<fontcolor="brown"><style>
+  *{
+    color:#5C4033;
+  }
+body {
+  background-image: url('https://i.pinimg.com/564x/ff/ce/ba/ffceba279f383f3720a790aa2fe66f91.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100%;
+}
+div.image{
+  margin: 50px;
+  border: 5px solid #B07233;
+  float: left;
+  width: 100x;
+}
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+
+</style>
+<h1>
+<font face="Times New Roman">
+<center>WELCOME TO THE FIRE GALLERY!</br></center></h1>
+<hr noshade color=#743600 size="5">
+<font face="Arial">
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILhq3h.1.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 1</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILhDtD.2.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 2</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILhUFC.3.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 3</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILhiRp.5.jpg" width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 4</div>
+</div>
+
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILiE2D.6.jpg" width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 5</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILh6sr.7.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 6</div>
+</div>
+
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILhPz0.8.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 7</div>
+</div>
+
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILhAjc.9.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 8</div>
+</div>
+
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILhgsy.10.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 9</div>
+</div>
+
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILhnyY.11.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 10</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILh4HD.12.jpg" width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 11</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILiTop.13.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 12</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILi210.14.jpg" width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 13</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILidZc.15.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 14</div>
+</div>
+
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILiUAy.16.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 15</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILi9KF.17.jpg" width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 16</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILi6cX.18.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 17</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILiNG8.19.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 18</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILiyCM.20.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 19</div>
+</div>
+
+<div class="image">
+    <img src="https://i.im.ge/2023/03/31/ILixw4.4.jpg"  width="300" height="300">
+  </a>
+  <div  class="desc"><b>GALLERY 20</div>
+</div>
+
+</div>
+</form>
+</body>
+</html>
+
